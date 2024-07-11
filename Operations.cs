85caca0a1acf2829abaa5e53d2225780e65cc51b
LordLucass <lucasnoalpottergithub@gmail.com>
2024-07-11 16:15:20 -0300

    Operations rework. Fix #1

--- Operations.cs
+++ Operations.cs
@@ -4,67 +4,89 @@ namespace MathQuiz
 {
     public static class Operations
     {
+        private static readonly Random rand = new Random();
+        private const int MAX_SIMPLE_DIFFICULTY = 3;
+        private const int MAX_ADVANCED_DIFFICULTY = 5;
+
         public static string GenerateQuestion(int difficulty)
         {
-            Random rand = new Random();
-            int operationType = rand.Next(1, difficulty > 3 ? 7 : 5); // if difficulty > 3 - ^ / √ 
-            int num1, num2;
-            string question = "";
-
-            switch (operationType)
+            int operationType = rand.Next(1, difficulty > MAX_SIMPLE_DIFFICULTY ? 7 : 5);
+            string question = operationType switch
             {
-                case 1:
-                    num1 = rand.Next(1, 10 * difficulty);
-                    num2 = rand.Next(1, 10 * difficulty);
-                    question = $"{num1} + {num2} = ?";
-                    break;
-                case 2:
-                    num1 = rand.Next(1, 10 * difficulty);
-                    num2 = rand.Next(1, num1);
-                    question = $"{num1} - {num2} = ?";
-                    break;
-                case 3:
-                    num1 = rand.Next(1, 10 * difficulty);
-                    num2 = rand.Next(1, 10 * difficulty);
-                    question = $"{num1} * {num2} = ?";
-                    break;
-                case 4:
-                    num2 = rand.Next(1, 10 * difficulty);
-                    num1 = num2 * rand.Next(1, 10 * difficulty);
-                    question = $"{num1} / {num2} = ?";
-                    break;
-                case 5:
-                    num1 = rand.Next(1, 10 * difficulty);
-                    question = $"√{num1 * num1} = ?";
-                    break;
-                case 6:
-                    num1 = rand.Next(1, 5);
-                    num2 = rand.Next(1, difficulty);
-                    question = $"{num1} ^ {num2} = ?";
-                    break;
-            }
+                1 => GenerateAdditionQuestion(difficulty),
+                2 => GenerateSubtractionQuestion(difficulty),
+                3 => GenerateMultiplicationQuestion(difficulty),
+                4 => GenerateDivisionQuestion(difficulty),
+                5 => GenerateSquareRootQuestion(difficulty),
+                6 => GenerateExponentiationQuestion(difficulty),
+                _ => throw new InvalidOperationException("Invalid operation type") // exception
+            };
             return question;
+        }
+
+        private static string GenerateAdditionQuestion(int difficulty)
+        {
+            int num1 = rand.Next(1, 10 * difficulty);
+            int num2 = rand.Next(1, 10 * difficulty);
+            return $"{num1} + {num2} = ?";
+        }
+
+        private static string GenerateSubtractionQuestion(int difficulty)
+        {
+            int num1 = rand.Next(1, 10 * difficulty);
+            int num2 = rand.Next(1, num1);
+            return $"{num1} - {num2} = ?";
+        }
+
+        private static string GenerateMultiplicationQuestion(int difficulty)
+        {
+            int num1 = rand.Next(1, 10 * difficulty);
+            int num2 = rand.Next(1, 10 * difficulty);
+            return $"{num1} * {num2} = ?";
+        }
+
+        private static string GenerateDivisionQuestion(int difficulty)
+        {
+            int num2 = rand.Next(1, 10 * difficulty);
+            int num1 = num2 * rand.Next(1, 10 * difficulty);
+            return $"{num1} / {num2} = ?";
+        }
+
+        private static string GenerateSquareRootQuestion(int difficulty)
+        {
+            int num1 = rand.Next(1, 10 * difficulty);
+            return $"√{num1 * num1} = ?";
+        }
+
+        private static string GenerateExponentiationQuestion(int difficulty)
+        {
+            int num1 = rand.Next(1, 5);
+            int num2 = rand.Next(1, difficulty);
+            return $"{num1} ^ {num2} = ?";
         }
 
         public static bool VerifyAnswer(string question, string userAnswer)
         {
             string[] parts = question.Split(new char[] { ' ', '=' }, StringSplitOptions.RemoveEmptyEntries);
-            int num1 = int.Parse(parts[0]);
-            int num2 = parts.Length > 2 ? int.Parse(parts[2]) : 0;
-            char operation = parts[1][0];
-
-            double correctAnswer = operation switch
+            if (int.TryParse(parts[0], out int num1) && 
+                (parts.Length <= 2 || int.TryParse(parts[2], out int num2)) &&
+                double.TryParse(userAnswer, out double userAnswerParsed))
             {
-                '+' => num1 + num2,
-                '-' => num1 - num2,
-                '*' => num1 * num2,
-                '/' => num1 / num2,
-                '√' => Math.Sqrt(num1),
-                '^' => Math.Pow(num1, num2),
-                _ => 0
-            };
+                char operation = parts[1][0];
+                double correctAnswer = operation switch
+                {
+                    '+' => num1 + num2,
+                    '-' => num1 - num2,
+                    '*' => num1 * num2,
+                    '/' => num1 / (double)num2,
+                    '√' => Math.Sqrt(num1),
+                    '^' => Math.Pow(num1, num2),
+                    _ => throw new InvalidOperationException("Invalid operation") // exception
+                };
 
-            return Math.Abs(correctAnswer - double.Parse(userAnswer)) < 0.001;
+                return Math.Abs(correctAnswer - userAnswerParsed) < 0.001;
+            }
+            return false;
         }
     }
 }
